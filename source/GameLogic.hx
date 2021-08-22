d6daa32b89d545d2c6e7c154442a0089e1fc38ce
y579 <Ianseth09@gmail.com>
2021-08-22 15:22:05 -0700

    fd

---
+++ source/GameLogic.hx
@@ -0,0 +1,15 @@
+package;
+
+import flixel.system.FlxSplash;
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxG;
+
+class GameLogic extends PlayState{
+
+    override public function update(elapsed:Float):Void
+        {
+            super.update();
+            
+    }
+}
